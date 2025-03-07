ce9104e573971855915c30e62bfa8a56f4eb4c8b
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-03-07 17:14:26 +0300

    feat: implement text field styles (outlined, filled, disabled)

--- src/components/TextField/TextField.stories.ts
+++ src/components/TextField/TextField.stories.ts
@@ -13,5 +13,65 @@ type Story = StoryObj<typeof TextField>;
 export const Primary: Story = {
   args: {
     label: "label",
+    color: "primary",
+    variant: "outlined",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: "label",
+    color: "secondary",
+    variant: "outlined",
+  },
+};
+
+export const Plain: Story = {
+  args: {
+    label: "label",
+    color: "plain",
+    variant: "outlined",
+  },
+};
+
+export const PrimaryFilled: Story = {
+  args: {
+    label: "label",
+    color: "primary",
+    variant: "filled",
+  },
+};
+
+export const SecondaryFilled: Story = {
+  args: {
+    label: "label",
+    color: "secondary",
+    variant: "filled",
+  },
+};
+
+export const PlainFilled: Story = {
+  args: {
+    label: "label",
+    color: "plain",
+    variant: "filled",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: "label",
+    color: "primary",
+    variant: "outlined",
+    disabled: true,
+  },
+};
+
+export const FilldeDisabled: Story = {
+  args: {
+    label: "label",
+    color: "plain",
+    variant: "filled",
+    disabled: true,
   },
 };
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,33 +1,79 @@
 import styles from "./TextField.module.css";
-
 import clsx from "clsx";
 import { TextFieldProps } from "./TextField.types";
+import { useEffect, useRef, useState } from "react";
 
 const TextField: React.FC<TextFieldProps> = ({
   label,
-  color,
-  type,
-  variant,
+  color = "primary",
+  type = "text",
+  variant = "outlined",
+  id,
+  onFocus,
+  onBlur,
   ...props
 }) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const labelClassName = clsx(
     "label",
-    styles["label"],
+    styles.label,
     color && styles[color],
     variant && styles[variant],
+    isFocused && styles.focused,
   );
+
   const inputClassName = clsx(
     "text-field",
-    styles["text-field"],
+    styles.textField,
+    color && styles[color],
+    variant && styles[variant],
+    isFocused && styles.focused,
+  );
+
+  const containerClassName = clsx(
+    "container",
+    styles.container,
     color && styles[color],
     variant && styles[variant],
   );
 
+  useEffect(() => {
+    if (props.value) {
+      setIsFocused(true);
+    }
+  }, [props.value]);
+
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(true);
+    onFocus?.(event);
+  };
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (inputRef.current && inputRef.current.value === "") {
+      setIsFocused(false);
+    }
+    onBlur?.(event);
+  };
+
   return (
-    <>
-      <label className={labelClassName}>{label}</label>
-      <input className={inputClassName} type={type} {...props} />
-    </>
+    <div className={containerClassName}>
+      <input
+        ref={inputRef}
+        id={id}
+        className={inputClassName}
+        type={type}
+        {...props}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+      {label && (
+        <label htmlFor={id} className={labelClassName}>
+          {label}
+        </label>
+      )}
+    </div>
   );
 };
 
