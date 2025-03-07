9686a3f584fba8b0acce1459e1e10c46b0543d33
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-03-07 14:05:34 +0300

    feat: add text field to storybook

---
+++ src/components/TextField/TextField.stories.ts
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { TextField } from "./TextField";
+
+const meta: Meta<typeof TextField> = {
+  title: "Components/TextField",
+  component: TextField,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TextField>;
+
+export const Primary: Story = {
+  args: {
+    label: "label",
+  },
+};
